baba0b83f519df4217f04335d999820de394b8a8
Antonio Gonzalez Restrepo <antonio.gonzalez@xpertgroup.co>
2021-08-09 20:40:39 -0500

    Creacion del NavBar responsive Mobile First como sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import GlobalStyle from "./globalStyles";
 import { NavBar } from "./components";
 
 export const App = () => {
   return (
-    <div>
-      <h1>Hola Mundo!</h1>
+    <Router>
+      <GlobalStyle />
       <NavBar />
-    </div>
+      <Switch>
+        <Route exact patch="/" component={NavBar} />
+      </Switch>
+    </Router>
   );
 };
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,16 +1,50 @@
-import React from "react";
-import { Nav, NavBarContainer, NavLogo, NavIcon } from "./NavBar.elements";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
+import {
+  Nav,
+  NavBarContainer,
+  NavLogo,
+  NavIcon,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks
+} from "./NavBar.elements";
 
 const NavBar = () => {
+  const [click, setClick] = useState(false);
+
+  const toggleClick = () => setClick(!click);
+
   return (
     <>
-      <Nav>
-        <NavBarContainer>
-          <NavLogo to="/">
-            <NavIcon />
-          </NavLogo>
-        </NavBarContainer>
-      </Nav>
+      <IconContext.Provider value={{ color: "#fff " }}>
+        <Nav>
+          <NavBarContainer>
+            <NavLogo to="/">
+              <NavIcon />
+              ULTRA
+            </NavLogo>
+            <MobileIcon onClick={toggleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </MobileIcon>
+            <NavMenu onClick={toggleClick} click={click}>
+              <NavItem>
+                <NavLinks to="/">Home</NavLinks>
+              </NavItem>
+
+              <NavItem>
+                <NavLinks to="/services">Services</NavLinks>
+              </NavItem>
+
+              <NavItem>
+                <NavLinks to="/products">Products</NavLinks>
+              </NavItem>
+            </NavMenu>
+          </NavBarContainer>
+        </Nav>
+      </IconContext.Provider>
     </>
   );
 };
